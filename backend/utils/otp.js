f78b1f02da985fabb297ee9deacea4666f3e7470
dakshindath <dakshindath4@gmail.com>
2025-05-12 14:15:47 +0530

    updated frontend and backend

---
+++ backend/utils/otp.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+module.exports = async (email, otp) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', 
+    auth: {
+      user: 'retom7887@gmail.com',
+      pass: 'ajoo cnuw dlwm jhhj', 
+    },
+    tls: {
+      rejectUnauthorized: false, // Allow self-signed certificates if needed
+    },
+  });
+
+  const mailOptions = {
+    from: 'retom7887@gmail.com',
+    to: email,
+    subject: 'Verification Code',
+    text: `Your verification code is: ${otp}`,
+    html: `
+      <div style="font-family: Arial, sans-serif; line-height: 1.5; font-size: 16px;">
+        <p>Your verification code is: <b>${otp}</b></p>
+        <p>If you did not request this code, please ignore this email.</p>
+      </div>
+    `,
+
+  };
+
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully');
+    return info;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    throw error;
+  }
+};
